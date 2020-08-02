01ccbe312670f2a12d37b0bcf5555c9cdcc095d8
beatrixyu <beatrixyu@126.com>
2020-08-02 16:27:14 +0200

    Updates

---
+++ precache-manifest.f9210d424f08ce3bae833bd28d7cb7b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d3b671ebad7c57ebfbdf9091dfd9240",
+    "url": "/paint-app-react/index.html"
+  },
+  {
+    "revision": "879fb6b1fb57fd23c230",
+    "url": "/paint-app-react/static/css/main.815066de.chunk.css"
+  },
+  {
+    "revision": "f9dde3a82f083bccb336",
+    "url": "/paint-app-react/static/js/2.e93c064f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/paint-app-react/static/js/2.e93c064f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "879fb6b1fb57fd23c230",
+    "url": "/paint-app-react/static/js/main.d1337787.chunk.js"
+  },
+  {
+    "revision": "176e24a14dcd48a4197c",
+    "url": "/paint-app-react/static/js/runtime-main.432f9ae1.js"
+  }
+]);
